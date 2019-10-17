88e05bb2269b924e881785bb1caa0fa9790a135a
unknown <matius.m22@gmail.com>
2019-10-17 13:04:54 -0500

    Bases de datos agregadas

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path'); //there is no hassle with url bars
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-mongoose.connect('mongodb://mongodb+srv://brindis:<brindissies>@cluster0-wyxdg.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser:true})
+mongoose.connect('mongodb://mongodb+srv://brindis:<brindissies>@cluster0-wyxdg.mongodb.net/test?retryWrites=true&w=majority')
     .then(db => console.log('Db connected'))
     .catch(err => console.log(err));
 
