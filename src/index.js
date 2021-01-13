cbd0b699e676f7cb0952cd439305f487f22a5fbf
Mateo Montanez Baron <mateomontabaron@gmail.com>
2021-01-13 09:09:27 -0500

    Editing index.js and index-routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path'); //there is no hassle with url bars
-// const mongoose = require('mongoose');
 const morgan = require('morgan');
 
-// mongoose.connect('mongodb://mongodb+srv://brindis:<brindissies>@cluster0-wyxdg.mongodb.net/test?retryWrites=true&w=majority')
-//     .then(db => console.log('Db connected'))
-//     .catch(err => console.log(err));
-
-//Importing routes
 const indexRoutes = require('./routes/index-routes.js')
 
 //settings
 app.set('port', process.env.PORT || 2000); // A la constante port le agrego el puerto
-app.set('views',path.join(__dirname,'views')); 
+app.set('views',path.join(__dirname)); 
+// app.set('views',path.join(__dirname,'views')); 
 app.set('view engine', 'ejs')// what template engine I'm going to use
 app.engine('html', require('ejs').renderFile); //render file from html to ejs
 //middlewares
--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home.html', { title: 'Home' });
+    res.render('index.html', { title: 'index' });
+});
+router.get('/home', (req, res) => {
+    res.render('views/home.html', { title: 'Home' });
 });
 router.get('/info', (req, res) => {
-    res.render('info.html', { title: 'Info' });
+    res.render('views/info.html', { title: 'Info' });
 });
 router.get('/carta', (req, res) => {
     const l_array = new Array();
-    res.render('carta.html', { l_array });
+    res.render('views/carta.html', { l_array });
 
 });
 
